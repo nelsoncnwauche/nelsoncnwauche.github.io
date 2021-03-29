20a1a705968e6c1823034ed0db625f9052b5b868
Nelson C. Nwauche <cnwauche@gmail.com>
2021-03-28 21:43:55 -0400

    Added template script & minor changes

---
+++ template.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+xdg-open https://vszhub.github.io/not-pure-poole/2020/09/29/welcome-to-not-pure-poole/
+
+xdg-open https://github.com/nelsoncnwauche/not-pure-poole/blob/master/_posts/2020-09-29-welcome-to-not-pure-poole.md
